5f4a6336577649f60669a28a2d77865ab12a257c
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2007-03-17 18:48:19 +0000

    sig_atomic_t for priority

--- logger.cpp
+++ logger.cpp
@@ -2,6 +2,7 @@
 #include "rest-utils.hpp"
 #include <boost/thread/once.hpp>
 #include <testsoon.hpp>
+#include <signal.h> // for sig_atomic_t
 
 #ifndef REST_LOGPIPE
 #define REST_LOGPIPE "/tmp/rest-logpipe"
@@ -11,9 +12,14 @@ using namespace rest::utils;
 
 class logger::impl {
 public:
+  int fd;
+  sig_atomic_t min_priority;
 };
 
-logger::logger() : p(new impl) {}
+logger::logger() : p(new impl) {
+  p->fd = 0;
+  p->min_priority = INFO;
+}
 
 static logger *instance;
 static boost::once_flag once = BOOST_ONCE_INIT;
@@ -25,6 +31,21 @@ void logger::init() {
 logger &logger::get() {
   boost::call_once(&init, once);
   return *instance;
+}
+
+void logger::set_minimum_priority(int priority) {
+  p->min_priority = priority;
+}
+
+void logger::log(
+    int priority,
+    char const *file,
+    int line,
+    std::string const &data)
+{
+  if (p->min_priority > priority)
+    return;
+  (void) file; (void) line; (void) data; // TODO
 }
 
 TEST() {
