868ba2edd3d2734e0bd34b56c3c3b4f5ccea390b
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2007-03-19 23:00:53 +0000

    fixed current_date_time

--- http-handler.cpp
+++ http-handler.cpp
@@ -32,6 +32,7 @@
 #include <testsoon.hpp>
 #include <iostream> // DEBUG
 #include <sstream>
+#include <iomanip> // setw()...
 
 namespace io = boost::iostreams;
 
@@ -348,12 +349,46 @@ namespace http {
 
     // Returns a string with the correct formated current Data and Time
     // see RFC 2616 3.3 Date/Time Formats
-    std::string current_date_time() {
-      time_t t = std::time(0x0);
-      // warning: the following functions are not thread safe!
-      //   see localtime_r and asctime_r
-      std::tm *ltime = std::localtime(&t);
-      return std::asctime(ltime);
+    std::string current_date_time() { 
+      time_t time_buf;
+      time(&time_buf);
+      tm gmtime;
+      gmtime_r(&time_buf, &gmtime);
+      std::stringstream out;
+      switch (gmtime.tm_wday) {
+      case 0: out << "Sun"; break;
+      case 1: out << "Mon"; break;
+      case 2: out << "Tue"; break;
+      case 3: out << "Wed"; break;
+      case 4: out << "Thu"; break;
+      case 5: out << "Fri"; break;
+      case 6: out << "Sat"; break;
+      }
+      out << ", ";
+      out << std::setfill('0');
+      out << std::setw(2) << gmtime.tm_mday << ' ';
+      switch (gmtime.tm_mon) {
+      case 0: out << "Jan"; break;
+      case 1: out << "Feb"; break;
+      case 2: out << "Mar"; break;
+      case 3: out << "Apr"; break;
+      case 4: out << "May"; break;
+      case 5: out << "Jun"; break;
+      case 6: out << "Jul"; break;
+      case 7: out << "Aug"; break;
+      case 8: out << "Sep"; break;
+      case 9: out << "Oct"; break;
+      case 10: out << "Nov"; break;
+      case 11: out << "Dec"; break;
+      }
+      out << ' ';
+      out << (1900 + gmtime.tm_year);
+      out << ' ';
+      out << std::setw(2) << gmtime.tm_hour << ':';
+      out << std::setw(2) << gmtime.tm_min << ':';
+      out << std::setw(2) << gmtime.tm_sec;
+      out << " GMT";
+      return out.str();
     }
   }
 
