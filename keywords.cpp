05b98805cb7da11246543f59b744419c0e495465
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2007-04-07 17:15:18 +0000

    gobby #gobby #11

--- keywords.cpp
+++ keywords.cpp
@@ -9,6 +9,8 @@
 #include <stdexcept>
 #include <sstream>
 #include <cstdio>
+#include <map>
+#include <set>
 #include <iostream>//DEBUG
 
 using namespace rest;
@@ -142,6 +144,83 @@ std::istream &keywords::read(std::string const &keyword, int index) {
 void keywords::set_entity(
     std::istream *entity, std::string const &content_type)
 {
+/*
+mal die BNF suchen und hier rein schreiben:
+
+------------------
+     boundary := 0*69<bchars> bcharsnospace
+
+     bchars := bcharsnospace / " "
+
+     bcharsnospace := DIGIT / ALPHA / "'" / "(" / ")" /
+                      "+" / "_" / "," / "-" / "." /
+                      "/" / ":" / "=" / "?"
+
+   Overall, the body of a "multipart" entity may be specified as
+   follows:
+
+     dash-boundary := "--" boundary
+                      ; boundary taken from the value of
+                      ; boundary parameter of the
+                      ; Content-Type field.
+
+     multipart-body := [preamble CRLF]
+                       dash-boundary transport-padding CRLF
+                       body-part *encapsulation
+                       close-delimiter transport-padding
+                       [CRLF epilogue]
+
+     transport-padding := *LWSP-char
+                          ; Composers MUST NOT generate
+                          ; non-zero length transport
+                          ; padding, but receivers MUST
+                          ; be able to handle padding
+                          ; added by message transports.
+
+     encapsulation := delimiter transport-padding
+                      CRLF body-part
+
+     delimiter := CRLF dash-boundary
+
+     close-delimiter := delimiter "--"
+
+     preamble := discard-text
+
+     epilogue := discard-text
+
+     discard-text := *(*text CRLF) *text
+                     ; May be ignored or discarded.
+
+     body-part := MIME-part-headers [CRLF *OCTET]
+                  ; Lines in a body-part must not start
+                  ; with the specified dash-boundary and
+                  ; the delimiter must not appear anywhere
+                  ; in the body part.  Note that the
+                  ; semantics of a body-part differ from
+                  ; the semantics of a message, as
+                  ; described in the text.
+
+     OCTET := <any 0-255 octet value>
+------------------
+*/
+
+/*
+also, wie sieht das denn dann so grob aus?
+
+[bla\r\n]
+--boundary                 \r\n
+Header\r\n
+Header\r\n
+[\r\n
+Daten]
+\r\n--boundary             \r\n
+Header\r\n
+[\r\n
+Daten]
+\r\n--boundary--            [\r\n
+bla]
+*/
+
   std::cout << "~~ " << content_type << std::endl;
 
   std::string type;
@@ -152,6 +231,7 @@ void keywords::set_entity(
   utils::parse_content_type(content_type, type, pset, params);
   std::cout << "~~ " << type << " ; " << params["boundary"] << std::endl;
 
+  // ist das eine passable boundary?
   std::string boundary = "\r\n--" + params["boundary"];
 
   while (entity->peek() != EOF) {
@@ -162,10 +242,14 @@ void keywords::set_entity(
     std::cout << "<<\n";
     char ch;
     while (filt.get(ch))
-      if (ch)
-        std::cout << ch;
+      if (ch== 0)
+        std::cout << "\\0";
+      else if (ch == '\r')
+        std::cout << "\\r";
+      else if (ch == '\n')
+        std::cout << "\\n\n";
       else
-        std::cout << "\0";
+        std::cout << ch;
     std::cout << "\n>>\n" << std::flush;
 
     filt.pop();
