5ce35dd8d2dc28d66ec0579644896d6711fa3bde
Rüdiger Sonderfeld <ruediger@c-plusplus.de>
2007-04-20 12:33:18 +0000

    implemented read_config and a small test-app (works fine except for std::copy... (istream-iterator ignores spaces! :-( )

--- config.cpp
+++ config.cpp
@@ -1,156 +1,221 @@
 #include <string>
+#include <cassert>
 #include <algorithm>
 #include <boost/ref.hpp>
 #include <boost/variant.hpp>
 #include <boost/filesystem/path.hpp>
 #include <boost/filesystem/fstream.hpp>
+#include <boost/filesystem/operations.hpp>
 #include <boost/multi_index/mem_fun.hpp>
 #include <boost/multi_index_container.hpp>
 #include <boost/multi_index/hashed_index.hpp>
 #include <boost/multi_index/key_extractors.hpp>
+
+#ifdef STANDALONE
+#include <iostream>
+#include <iomanip>
+#endif
 
 using namespace boost::multi_index;
 namespace fs = boost::filesystem;
 
 namespace rest {
 namespace utils {
-  namespace {
-    class property {
-      std::string const name_;
+  class property {
+    std::string const name_;
 
-      class data_handler {
-      public:
-        data_handler(fs::path const &file)
-          : data_(file)
-        { }
-        data_handler(std::string const &data)
-          : data_(data)
-        { }
-
-        std::string const &data() const {
-          std::string const *data;
-          if( (data = boost::get<std::string>(&data_)) )
-            return *data;
-          else {
-            read_data();
-            return boost::get<std::string>(data_);
-          }
-        }
-      private:
-        void read_data() const {
-          fs::path const &file = boost::get<fs::path>(data_);
-          std::string data;
-
-          fs::ifstream in(file);
-          std::copy(std::istream_iterator<char>(in),
-                    std::istream_iterator<char>(),
-                    std::back_inserter(data));
-
-          data_ = boost::variant<fs::path, std::string>(data);
-        }
-
-        mutable boost::variant<fs::path, std::string> data_;
-      } const data_;
+    class data_handler {
     public:
-      property(std::string const &name,
-               fs::path const &file)
-        : name_(name), data_(file)
+      explicit data_handler(fs::path const &file)
+        : data_(file)
+      { }
+      explicit data_handler(std::string const &data)
+        : data_(data)
       { }
 
-      property(std::string const &name,
-               std::string const &data)
-        : name_(name), data_(data)
-      { }
-
-      std::string const &name() const {
-        return name_;
-      }
       std::string const &data() const {
-        return data_.data();
-      }
-    };
-
-    namespace utils {
-      template<class Class,typename Type,
-               Type const &(Class::*PtrToMemberFunction)() const>
-      struct ref_const_mem_fun_const {
-        typedef typename boost::remove_reference<Type>::type result_type;
-
-        template<typename ChainedPtr>
-        Type const &operator()(ChainedPtr const& x) const {
-          return operator()(*x);
+        std::string const *data;
+        if( (data = boost::get<std::string>(&data_)) )
+          return *data;
+        else {
+          read_data();
+          return boost::get<std::string>(data_);
         }
-
-        Type const &operator()(Class const& x) const {
-          return (x.*PtrToMemberFunction)();
-        }
-
-        Type const &operator()
-          (boost::reference_wrapper<Class const> const& x) const
-        { 
-          return operator()(x.get());
-        }
-
-        Type const &operator()
-          (boost::reference_wrapper<Class> const& x,int=0) const
-        { 
-          return operator()(x.get());
-        }
-      };
-
-#define REF_CONST_MEM_FUN_CONST(Class, Type, MemberFunName)   \
-      utils::ref_const_mem_fun_const<Class, Type, &Class::MemberFunName>
-    }
-
-    class property_tree {
-    public:
-      std::string const &name() const {
-        return name_;
       }
     private:
-      typedef boost::multi_index_container<
-        property_tree*,
-        indexed_by<
-          hashed_unique<
-            REF_CONST_MEM_FUN_CONST(property_tree, std::string, name)
-            >
-          >
-         > children_t;
+      void read_data() const {
+        fs::path const &file = boost::get<fs::path>(data_);
+        std::string data;
 
-      typedef boost::multi_index_container<
-        property,
-        indexed_by<
-          hashed_unique<
-            REF_CONST_MEM_FUN_CONST(property, std::string, name)
-            >
-          >
-         > property_t;
+        fs::ifstream in(file);
+        std::copy(std::istream_iterator<char>(in),
+                  std::istream_iterator<char>(),
+                  std::back_inserter(data));
 
-      children_t children;
-      property_t properties;
-      std::string name_;
-    public:
-      typedef property_t::const_iterator property_iterator;
-      property_iterator property_begin() const {
-        return properties.begin();
-      }
-      property_iterator property_end() const {
-        return properties.end();
-      }
-      property_iterator find_property(std::string const &name) const {
-        return properties.find(name);
+        data_ = boost::variant<fs::path, std::string>(data);
       }
 
-      typedef children_t::const_iterator children_iterator;
-      children_iterator children_begin() const {
-        return children.begin();
+      mutable boost::variant<fs::path, std::string> data_;
+    } const data_;
+  public:
+    property(std::string const &name,
+             fs::path const &file)
+      : name_(name), data_(file)
+    { }
+
+    property(std::string const &name,
+             std::string const &data)
+      : name_(name), data_(data)
+    { }
+
+    std::string const &name() const {
+      return name_;
+    }
+    std::string const &data() const {
+      return data_.data();
+    }
+  };
+
+  namespace {
+    template<class Class,typename Type,
+             Type const &(Class::*PtrToMemberFunction)() const>
+    struct ref_const_mem_fun_const {
+      typedef typename boost::remove_reference<Type>::type result_type;
+
+      template<typename ChainedPtr>
+      Type const &operator()(ChainedPtr const& x) const {
+        return operator()(*x);
       }
-      children_iterator children_end() const {
-        return children.end();
+
+      Type const &operator()(Class const& x) const {
+        return (x.*PtrToMemberFunction)();
       }
-      children_iterator find_children(std::string const &name) const {
-        return children.find(name);
+
+      Type const &operator()
+        (boost::reference_wrapper<Class const> const& x) const { 
+        return operator()(x.get());
+      }
+
+      Type const &operator()
+        (boost::reference_wrapper<Class> const& x,int=0) const { 
+        return operator()(x.get());
       }
     };
+
+#define REF_CONST_MEM_FUN_CONST(Class, Type, MemberFunName)             \
+    utils::ref_const_mem_fun_const<Class, Type, &Class::MemberFunName>
+  }
+
+  class property_tree {
+  public:
+    std::string const &name() const {
+      return name_;
+    }
+  private:
+    typedef boost::multi_index_container<
+    property_tree*,
+    indexed_by<
+      hashed_unique<
+        REF_CONST_MEM_FUN_CONST(property_tree, std::string, name)
+        >
+      >
+    > children_t;
+
+    typedef boost::multi_index_container<
+      property,
+      indexed_by<
+        hashed_unique<
+          REF_CONST_MEM_FUN_CONST(property, std::string, name)
+          >
+        >
+      > property_t;
+
+    children_t children;
+    property_t properties;
+    std::string const name_;
+
+    explicit property_tree(std::string const &name)
+      : name_(name)
+    { }
+
+    void add_child(property_tree *child) {
+      assert(child);
+      children.insert(child);
+    }
+
+    void add_property(property const &p) {
+      properties.insert(p);
+    }
+
+    friend property_tree read_config(fs::path const &path);
+  public:
+    typedef property_t::const_iterator property_iterator;
+    property_iterator property_begin() const {
+      return properties.begin();
+    }
+    property_iterator property_end() const {
+      return properties.end();
+    }
+    property_iterator find_property(std::string const &name) const {
+      return properties.find(name);
+    }
+    
+    typedef children_t::const_iterator children_iterator;
+    children_iterator children_begin() const {
+      return children.begin();
+    }
+    children_iterator children_end() const {
+      return children.end();
+    }
+    children_iterator find_children(std::string const &name) const {
+      return children.find(name);
+    }
+
+    ~property_tree() {
+      for(children_iterator i = children.begin();
+          i != children.end();
+          ++i)
+        delete *i;
+    }
+  };
+
+  property_tree read_config(fs::path const &path) {
+    property_tree root(path.leaf());
+    fs::directory_iterator end_iter;
+    for(fs::directory_iterator iter(path);
+        iter != end_iter;
+        ++iter)
+    {
+      if(fs::is_directory(*iter))
+        root.add_child(new property_tree(read_config(*iter)));
+      else
+        root.add_property(property(iter->leaf(), *iter));
+    }
+    return root;
   }
 }}
+
+#ifdef STANDALONE
+using namespace rest::utils;
+
+void print_tree(property_tree const &ref, unsigned depth = 0) {
+  std::string align(' ', depth * 2);
+  std::cout << align << "{ " << ref.name() << "\n";
+  for(property_tree::property_iterator i = ref.property_begin();
+      i != ref.property_end();
+      ++i)
+    std::cout << align << "  " << i->name() << " => `"
+              << i->data() << "'\n";
+  ++depth;
+  for(property_tree::children_iterator i = ref.children_begin();
+      i != ref.children_end();
+      ++i)
+    print_tree(**i, depth);
+  std::cout << align << "}\n";
+}
+
+int main() {
+  property_tree t = read_config("/tmp/example");
+  print_tree(t);
+}
+#endif
