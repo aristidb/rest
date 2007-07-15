6a2133e4791396edf088304501fc0c37ec81a6dc
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2007-07-15 23:46:44 +0000

    fixed cpu usage

--- pipedump.cpp
+++ pipedump.cpp
@@ -12,19 +12,25 @@ int main(int argc, char **argv) {
     std::cerr << "usage: " << argv[0] << " <fifo>\n";
     return 1;
   }
+start:
   int pipe = open(argv[1], O_RDONLY);
   if (pipe < 0) {
     std::cerr << argv[0] << ": Could not open pipe " << argv[1]
               << ": `" << strerror(errno) << "'\n";
     return 2;
   }
+
   char buf[4096];
   ssize_t n;
   for (;;) {
     n = read(pipe, buf, sizeof(buf));
-    if (n < 0) {
+    if (n < 0)
       break;
+    else if (n == 0) {
+      close(pipe);
+      goto start;
     }
     std::cout.write(buf, n);
+    std::cout.flush();
   }
 }
