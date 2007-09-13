65ccebeddbda573cfa41dca407975ee88b412f6c
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2007-09-13 21:43:56 +0000

    fixed auto-merge

--- response.cpp
+++ response.cpp
@@ -338,7 +338,6 @@ void response::print_cookie_header(std::ostream &out) const {
   for(cookie_iterator i = begin; i != end; ++i) {
     print_cookie(out, *i);
   }
-  out << "\r\n";
 }
 
 void response::print_headers(std::ostream &out) const {
