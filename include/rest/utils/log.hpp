d2c13f66718a15978f98a71cdd550a3026107d55
Aristid Breitkreuz <aristid.breitkreuz@gmx.de>
2008-04-05 13:45:34 +0000

    added priority field to log_record (wow, this changes so often)

--- include/rest/utils/log.hpp
+++ include/rest/utils/log.hpp
@@ -11,7 +11,7 @@ namespace rest { namespace utils {
 
 void log(int priority, char const *message, ...);
 
-typedef std::vector<std::pair<std::string, std::string> > log_record;
+struct log_record;
 
 class logger {
 public:
@@ -20,12 +20,28 @@ public:
   virtual void log(log_record const &) = 0;
 };
 
-template<class T>
-void add_field(log_record &rec, std::string const &field, T const &value) {
-  std::ostringstream o;
-  o << value;
-  rec.push_back(std::make_pair(field, o.str()));
-}
+enum log_priority {
+  LOG_NORMAL
+};
+
+struct log_record {
+  typedef std::pair<std::string, std::string> field_type;
+  typedef std::vector<field_type> fields_type;
+
+  fields_type fields;
+  log_priority priority;
+
+  log_record(log_priority priority = LOG_NORMAL)
+  : priority(priority)
+  {}
+
+  template<class T>
+  void add_field(std::string const &field, T const &value) {
+    std::ostringstream o;
+    o << value;
+    fields.push_back(field_type(field, o.str()));
+  }
+};
 
 }}
 
